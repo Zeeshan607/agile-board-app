b31fb4b7c95ef1c24b337db2630f841208cdecfb
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-09-29 12:42:22 +0500

    connected db.js file to .env variables

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 
 
 
-const sequelize = new Sequelize('kanban-board', 'root', '', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
         host: process.env.MYSQL_HOST, 
         dialect: 'mysql',
         pool: {
