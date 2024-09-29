955307cbeab69a168179a824a86e35a2592362ad
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-09-29 13:02:39 +0500

    changed api request end point setting in vite.config file

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1'),
       },
     },
   },
