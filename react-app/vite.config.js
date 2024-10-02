e9c3d527c5b1a08afa64f85d62123808c2e5212b
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-10-02 17:23:56 +0500

    saperated production and development environment in vite config file

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -2,32 +2,91 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port:parseInt(process.env.VITE_PORT) || 4173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        // rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Split each dependency into its own chunk
-            return id.split('node_modules/')[1].split('/')[0];
-          }
-          if (id.includes('froala-editor')) {
-            return 'froala-editor';
-          }
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port:parseInt(process.env.VITE_PORT) || 4173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000/api',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//         // rewrite: (path) => path.replace(/^\/api/, '/api'),
+//       },
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             // Split each dependency into its own chunk
+//             return id.split('node_modules/')[1].split('/')[0];
+//           }
+//           if (id.includes('froala-editor')) {
+//             return 'froala-editor';
+//           }
+//         },
+//       },
+//     },
+//   },
+// })
+
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve' || command === 'preview' || command === 'dev' ) {
+           // development config
+    return {
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:5000/api',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
         },
       },
-    },
-  },
-})
+      build: {
+            rollupOptions: {
+              output: {
+                manualChunks(id) {
+                  if (id.includes('node_modules')) {
+                    // Split each dependency into its own chunk
+                    return id.split('node_modules/')[1].split('/')[0];
+                  }
+                  if (id.includes('froala-editor')) {
+                    return 'froala-editor';
+                  }
+                },
+              },
+            },
+          },
+    };
+
+
+  } else {
+
+
+       // Production config
+    return {
+ 
+      plugins: [react()],
+      build: {
+          rollupOptions: {
+            output: {
+              manualChunks(id) {
+                if (id.includes('node_modules')) {
+                  // Split each dependency into its own chunk
+                  return id.split('node_modules/')[1].split('/')[0];
+                }
+                if (id.includes('froala-editor')) {
+                  return 'froala-editor';
+                }
+              },
+            },
+          },
+        },
+    };
+  }
+});
