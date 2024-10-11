70b3f2ddd7aa425282bc1d6a59331db66ff60f3c
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-10-11 17:00:22 +0500

    change users table's u to uppercase in usermodal backend

--- models/Workspace.js
+++ models/Workspace.js
@@ -30,7 +30,7 @@ const Workspace =sequelize.define('Workspace',{
       type:DataTypes.INTEGER,
       allowNull:false,
       references:{
-        model:'users',
+        model:'Users',
         key:'id'
       }
     },
